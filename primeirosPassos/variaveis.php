a7e41c932b168aada56d70ca498b0dea0a2a98bd
liliane <cpaixaoliliane@gmail.com>
2023-04-29 17:25:47 -0300

    8 tipos de dados do php

---
+++ primeirosPassos/variaveis.php
@@ -0,0 +1,35 @@
+<?php
+
+// $nome = "Hcode";
+
+// echo $nome;
+
+// var_dump($nome);
+
+//$nascimento = new DateTime();
+
+//var_dump($nascimento);
+
+//$arquivo = fopen("for.php", "r");
+
+//var_dump($arquivo);
+
+//$nulo = NULL;
+
+//$nome = (int)$_GET["a"];
+
+//var_dump($nome);
+
+//$ip = $_SERVER["SCRIPT_NAME"];
+
+//echo $ip;
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+echo $ip;
+
+
+
+
+
+?>
